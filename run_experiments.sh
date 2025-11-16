33b71a4c4e348f16599f74a20c87a1e64f779591
Cxrniani <matheuscorniani@gmail.com>
2025-11-16 12:17:53 -0300

    fix: removing misplaced "

--- run_experiments.sh
+++ run_experiments.sh
@@ -8,7 +8,7 @@ mkdir -p results
 echo "Executando Experimento de Threads..."
 for i in {1..5}
 do
-    python3 threads/producer_consumer.py | grep ""Tempo de execucao" | awk '{print $4}' >> results/threads_times.csv
+    python3 threads/producer_consumer.py | grep "Tempo de execucao" | awk '{print $4}' >> results/threads_times.csv
 done
 
 echo "Executando Experimento de Coroutines..."
@@ -22,5 +22,5 @@ echo "Fim da Execução."
 echo "Gerando Gráfico de Resultados..."
 
 python3 plot_results.py
-
+ 
 echo "Gráfico gerado em results/comparacao_performance.png"
